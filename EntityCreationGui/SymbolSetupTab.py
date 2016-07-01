bbef6beb470672b83dbd6740c5cfc022d3e7af98
akhier <akherthedragonhearted@gmail.com>
2016-07-01 12:10:13 -0400

    +SELECT_SYMBOL

--- EntityCreationGui/SymbolSetupTab.py
+++ EntityCreationGui/SymbolSetupTab.py
@@ -31,6 +31,42 @@ class SymbolSetup(tk.Frame):
             'NAME_BRIDGE', 'NAME_WALL', 'NAME_BUILDING_TOMB',
             'NAME_BUILDING_LIBRARY', 'NAME_FESTIVAL']
 
+        # SELECT_SYMBOL
+        self.symboldict = {}
+        self.setsymbol = tk.Button(self, text='Set Symbol',
+                                   command=lambda: self.add_symbol())
+        self.setsymbol.grid(row=0, column=0, sticky=('w', 'e'))
+        self.symbolnoun = ttk.Combobox(self, values=self.symbolnounlist)
+        self.symbolnoun.grid(row=1, column=0, sticky=('w', 'e'))
+        self.symbolnoun.set('ALL')
+        self.symbol = ttk.Combobox(self, values=self.symbollist)
+        self.symbol.grid(row=2, column=0, sticky=('w', 'e'))
+        self.symbol.set('FLOWERY')
+        self.removesymbol = tk.Button(self, text='Remove Symbol',
+                                      command=lambda: self.remove_symbol())
+        self.removesymbol.grid(row=3, column=0, sticky=('w', 'e'))
+        self.symbols = tkst.ScrolledText(self, state='disabled', height=5,
+                                         width=50, wrap=tk.WORD)
+        self.symbols.grid(row=0, column=1, rowspan=4,
+                          columnspan=4, sticky=('w', 'e'))
+
+    def add_symbol(self):
+        self.symboldict[self.symbol.get()] = self.symbolnoun.get()
+        self.fill_symbols()
+
+    def remove_symbol(self):
+        if self.symbol.get() in self.symboldict:
+            self.symboldict.pop(self.supportedbiome.get(), None)
+        self.fill_symbols()
+
+    def fill_symbols(self):
+        string = ''
+        for key, value in self.symboldict.items():
+            string = string + value + ':' + key + ' '
+        self.symbols.config(state='normal')
+        self.symbols.delete('1.0', tk.END)
+        self.symbols.insert('1.0', string)
+        self.symbols.config(state='disabled')
 
 if __name__ == '__main__':
     root = tk.Tk()
